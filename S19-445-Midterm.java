2120dcbcf6273b8647b9b3032210b5f3078dc050
avigloz <avi@avigloz.net>
2019-02-27 18:17:36 -0500

    Added some misc notes for midterm, and the midterm responses file.

---
+++ S19-445-Midterm.java
@@ -0,0 +1,27 @@
+/* LEAVE THIS COMMENT HERE
+MY NAME: <JOHN F KENNEDY>  MY PITT EMAIL ID: <JFK63>  I TOOK MY CS/COE 445 EXAM: <3PM> or <6PM> or  <DRS TESTING ROOM>
+
+#1	A
+#2	D
+#3	A
+#4	D
+#5	A
+#6	D
+#7  B
+#8  A
+#9	B
+#10 D
+#11	B
+#12	B
+#13	B
+#14	B
+#15	E
+#16	C
+#17	D
+#18	E
+#19	A
+#20	D
+#21	In order to avoid having to form every single possible string from the board, which would take way too long.
+#22	So there is more than enough space for incoming elements. It's too inefficient to upSize every single time, when instead upSize will eventually become unecessary.
+
+LEAVE THIS COMMENT HERE */
